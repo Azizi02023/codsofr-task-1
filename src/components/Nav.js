c7270ea1c5f4fbd2e93e664d8338ecca38394693
Azizi02023 <mohammadatif.azizi@gmail.com>
2024-05-21 15:58:51 +0500

    Initial commit

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -13,23 +13,23 @@ class Nav extends Component {
     render() {
         return (
             <div className='main'>
-            <nav className='navbaritems'>
-                <h1 className='logo'><span>Azizi</span>Travel</h1>
-                <div className='menu-icons' onClick={this.handleClick}>
-                    <i className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}></i>
-                </div>
-                <ul className={this.state.clicked ? 'nav-menu active' : 'nav-menu'}>
-                    {Menuitems.map((item, index) => (
-                        <li key={index}>
-                            <Link className={item.cName} to={item.url}>
-                                <i className={item.icon}></i>
-                                {item.title}
-                            </Link>
-                        </li>
-                    ))}
-                    <button>Sign Up</button>
-                </ul>
-            </nav>
+                <nav className='navbaritems'>
+                    <h1 className='logo'><span>Azizi</span>Travel</h1>
+                    <div className='menu-icons' onClick={this.handleClick}>
+                        <i className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}></i>
+                    </div>
+                    <ul className={this.state.clicked ? 'nav-menu active' : 'nav-menu'}>
+                        {Menuitems.map((item, index) => (
+                            <li key={index}>
+                                <Link className={item.cName} to={item.url}>
+                                    <i className={item.icon}></i>
+                                    {item.title}
+                                </Link>
+                            </li>
+                        ))}
+                        <button>Sign Up</button>
+                    </ul>
+                </nav>
             </div>
         );
     }
